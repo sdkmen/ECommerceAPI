b7e1236ae303e25977d371fb7dfa6fe4d1c0dcfd
Seyfullah Dikmen <seyfullah_dikmen@hotmail.com>
2023-03-08 23:27:48 +0300

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get {
+                ConfigurationManager configurationManager = new();
+                //appsettings.json'a gitmek için path oluşturuyoruz, Directory.GetCurrentDirectory() bize içinde bulunduğumuz ECommerceAPI.Persistence klasörünü veriyor ../ ile bir dışa Infrastructure'a çıkıyoruz ../ ile solutiona ulaşıp devamında ECommerceAPI.API'ye giriyoruz.
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ECommerceAPI.API"));
+                //Microsoft.Extensions.Configurations.Json kütüphanesi sayesinde addJsonFile'a ulaşabiliyoruz.
+                configurationManager.AddJsonFile("appsettings.json");
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }    
+        }
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
@@ -0,0 +1,44 @@
+﻿    using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Contexts
+{
+    public class ECommerceAPIDbContext : DbContext
+    {
+        public ECommerceAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker: Entityler üzerineden yapılan değişikliklerin ya da yeni eklenen verilerin yakalanmasını sağlayan propertydir.
+            //Update operasyonlarında track edilen verileri yakalayıp elde etmemizi sağlar.
+            //Insert işleminde veri zaten yeni oluşturulduğu için track işlemi mümkün değildir.
+            var datas = ChangeTracker
+                .Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                //_ discard operator
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow
+                };
+            }
+            
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
