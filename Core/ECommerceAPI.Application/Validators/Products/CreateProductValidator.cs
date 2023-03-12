cabef6c07b0c48f0d3c317557443da73637367f6
Seyfullah Dikmen <seyfullah_dikmen@hotmail.com>
2023-03-12 13:23:19 +0300

    Add fluent validation and validation filter

---
+++ Core/ECommerceAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Validators.Products
+{
+    public class CreateProductValidator: AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                    .WithMessage("Name can not be empty.")
+                .NotNull()
+                    .WithMessage("Name can not be null.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                    .WithMessage("Name lenght must be between 5 and 150.");
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                    .WithMessage("Stock can not be empty.")
+                .NotNull()
+                    .WithMessage("Stock can not be null.")
+                .Must(s => s >= 0)
+                    .WithMessage("Stock can not be negative");
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                    .WithMessage("Price can not be empty.")
+                .NotNull()
+                    .WithMessage("Price can not be null.")
+                .Must(s => s >= 0)
+                    .WithMessage("Price can not be negative");
+        }
+    }
+}
--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -36,7 +36,10 @@ namespace ECommerceAPI.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
+            if(ModelState.IsValid)
+            {
 
+            }
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
