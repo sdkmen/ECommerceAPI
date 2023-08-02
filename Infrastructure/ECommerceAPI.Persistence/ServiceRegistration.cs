486560831db258143188ac5ea1156890921f60f9
Seyfullah Dikmen <sdkmenn@gmail.com>
2023-08-03 00:19:56 +0300

    Add microsoft.identity and creating user operations

--- Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -9,15 +9,25 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using ECommerceAPI.Application.Repositories;
 using ECommerceAPI.Persistence.Repositories;
+using ECommerceAPI.Domain.Entities.Identity;
 
 namespace ECommerceAPI.Persistence
 {
     public static class ServiceRegistration
     {
-       
-        public static void AddPersistenceServices(this IServiceCollection services) 
+
+        public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ECommerceAPIDbContext>(options=>options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<ECommerceAPIDbContext>();
+
             services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
             services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
             services.AddScoped<IOrderReadRepository, OrderReadRepository>();
