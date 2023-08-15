db891a07f065ca9b33687016ea7d57448988910e
Seyfullah Dikmen <sdkmenn@gmail.com>
2023-08-16 00:38:48 +0300

    Add facebook login

---
+++ Core/ECommerceAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,92 @@
+﻿using ECommerceAPI.Application.Abstractions.Token;
+using ECommerceAPI.Application.DTOs;
+using ECommerceAPI.Application.DTOs.Facebook;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+    public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+        readonly HttpClient _httpClient;
+
+        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IHttpClientFactory httpClientFactory)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/acccess_token?client_id=115348721656067&client_secret=e6b50ee5d8466cb9a0d9177e5f1707b8&grant_type=client_credentials");
+
+            FacebookAccessTokenResponse facebookAccessTokenResponse_DTO = JsonSerializer.Deserialize<FacebookAccessTokenResponse>(accessTokenResponse);
+
+            string userAccessTokenValidation = await _httpClient.GetStringAsync($"https://graph.facebook.com/debug_token?input_token={request.AuthToken}&access_token={facebookAccessTokenResponse_DTO.AccessToken}");
+
+            FacebookUserAccessTokenValidation validation = JsonSerializer.Deserialize<FacebookUserAccessTokenValidation>(userAccessTokenValidation);
+
+            if (validation.Data.IsValid)
+            {
+                string userInfoResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=email,name&access_token={request.AuthToken}");
+
+                FacebookUserInfoResponse userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK");
+
+                var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                bool result = user != null;
+                if (user == null)
+                {
+                    //user = await _userManager.FindByEmailAsync(payload.Email);
+                    //if (user == null)
+                    //{
+                    //    user = new()
+                    //    {
+                    //        Id = Guid.NewGuid().ToString(),
+                    //        Email = payload.Email,
+                    //        UserName = payload.Email,
+                    //        NameSurname = payload.Name
+                    //    };
+                    //    var identityResult = await _userManager.CreateAsync(user);
+                    //    result = identityResult.Succeeded;
+                    //}
+
+                    user = new()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = userInfo.Email,
+                        UserName = userInfo.Email,
+                        NameSurname = userInfo.Name
+                    };
+                    var identityResult = await _userManager.CreateAsync(user);
+                    result = identityResult.Succeeded;
+                }
+
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(user, info); //adds current user to AspNetUserLogins table 
+  
+                    Token token = _tokenHandler.CreateAccessToken(5);
+
+                    return new()
+                    {
+                        Token = token
+                    };
+                }
+
+            }
+            throw new Exception("Invalid external authentication.");
+        }
+    }
+}
--- Presentation/ECommerceAPI.API/Controllers/UsersController.cs
+++ Presentation/ECommerceAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Features.Commands.AppUser.CreateUser;
+using ECommerceAPI.Application.Features.Commands.AppUser.FacebookLogin;
 using ECommerceAPI.Application.Features.Commands.AppUser.GoogleLogin;
 using ECommerceAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
@@ -35,6 +36,13 @@ namespace ECommerceAPI.API.Controllers
         public async Task<IActionResult> GoogleLogin(GoogleLoginCommandRequest googleLoginCommandRequest)
         {
             GoogleLoginCommandResponse response = await _mediator.Send(googleLoginCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("facebook-login")]
+        public async Task<IActionResult> FacebookLogin(FacebookLoginCommandRequest facebookLoginCommandRequest)
+        {
+            FacebookLoginCommandResponse response = await _mediator.Send(facebookLoginCommandRequest);
             return Ok(response);
         }
     }
